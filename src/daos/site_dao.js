6124448488d627b19ee1cf64520b3ae196c15ad2
Efraín González <efgonzalezca@gmail.com>
2023-10-31 08:31:25 -0500

    add Hands-On Activity week 3

--- src/daos/site_dao.js
+++ src/daos/site_dao.js
@@ -1,7 +1,7 @@
 const daoLoader = require('./daoloader');
 
 // Week 3, change this from 'site' to 'site_geo'.
-const impl = daoLoader.loadDao('site');
+const impl = daoLoader.loadDao('site_geo');
 
 module.exports = {
   /**
