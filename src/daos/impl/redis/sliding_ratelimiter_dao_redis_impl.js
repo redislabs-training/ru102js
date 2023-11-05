8f2288dc4f1c2fc9e2c6090f6bb703166bb041df
Efraín González <efgonzalezca@gmail.com>
2023-11-05 13:35:02 -0500

    add challenge 7

--- src/daos/impl/redis/sliding_ratelimiter_dao_redis_impl.js
+++ src/daos/impl/redis/sliding_ratelimiter_dao_redis_impl.js
@@ -11,7 +11,30 @@ const hitSlidingWindow = async (name, opts) => {
   const client = redis.getClient();
 
   // START Challenge #7
-  return -2;
+  const key = keyGenerator.getKey(`limiter:${opts.interval}:${name}:${opts.maxHits}`);
+  const now = timeUtils.getCurrentTimestampMillis();
+
+  const transaction = client.multi();
+
+  const member = `${now}-${Math.random()}`;
+
+  transaction.zadd(key, now, member);
+  transaction.zremrangebyscore(key, 0, now - opts.interval);
+  transaction.zcard(key);
+
+  const response = await transaction.execAsync();
+
+  const hits = parseInt(response[2], 10);
+
+  let hitsRemaining;
+
+  if (hits > opts.maxHits) {
+    hitsRemaining = -1;
+  } else {
+    hitsRemaining = opts.maxHits - hits;
+  }
+
+  return hitsRemaining;
   // END Challenge #7
 };
 
